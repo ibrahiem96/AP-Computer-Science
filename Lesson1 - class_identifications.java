591a643971c1aeb6aafbdbb3d9d3cb75e3842e93
Ibrahiem Mohammad <ibrahiem.mohammed96@gmail.com>
2015-09-19 11:44:10 -0500

    add Lesson 1.txt

---
+++ Lesson1 - class_identifications.java
@@ -0,0 +1,20 @@
+
+public class Lesson1 {
+
+	public static void main (String[] args){		//main method
+		
+		car jeep = new car();
+		
+	}
+	
+}
+
+class car {											//define class car
+	int wheels;
+	int doors;
+	double fuelCapacity;
+	char logo;
+}
+
+
+
